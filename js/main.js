78faa8362a257adba9abb9a3c812d708498a463a
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-02-23 16:55:01 +0100

    fine bonus1 e bonus2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 'use strict';
 
+const row=document.getElementById("row");
 let output;
 
 for (let i=1;i<=100;i++){
+    const col=document.createElement("div");
+    col.classList.add("col-2");
     output=""+i;
-    if (i%3 === 0 && i%5 === 0) output="FizzBuzz";
+    if (i%3 === 0 && i%5 === 0){
+        output="FizzBuzz";
+        col.classList.add("fizzbuzz");
+    } 
     else{
-        if (i%3 === 0)  output="Fizz";
-        if (i%5 === 0)  output="Buzz";
+        if (i%3 === 0){
+            output="Fizz";
+            col.classList.add("fizz");
+        }  
+        if (i%5 === 0){
+            output="Buzz";
+            col.classList.add("buzz");
+        }  
     }
     console.log(i+": "+output);
+    
+    col.append(output);
+    row.append(col);
 }
